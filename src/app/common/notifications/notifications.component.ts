dd1445c293f867179874e926107ea6da8de32a85
Louis Walter <louis.walter0725@gmail.com>
2019-08-07 20:15:01 +0300

    Some changes

--- src/app/common/notifications/notifications.component.ts
+++ src/app/common/notifications/notifications.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input, NgZone, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { ApiService } from '@app/services/api.service';
-import { Store } from '@ngrx/store';
-import * as fromStore from '@app/store';
-import * as fromAccount from '@app/store/actions';
+import { SessionService } from '@app/services/session.service';
 
 @Component({
   selector: 'app-notifications',
@@ -14,30 +12,24 @@ export class NotificationsComponent implements OnInit, OnChanges {
   @Input() public user;
   public messages = [];
 
-  constructor(private msgService: MessageService, private api: ApiService, private _store: Store<fromStore.State>) {}
+  constructor(private msgService: MessageService, private api: ApiService, private sessionService: SessionService) {}
 
   ngOnInit() {
     this.messages = this.user ? this._mapMessages(this.user['notifications']) : [];
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['user']['notifications']) {
-      this.messages = this._mapMessages(changes['user']);
+    if (changes['user'] && changes['user']['notifications']) {
+      this.messages = this._mapMessages(changes['user']['notifications']);
     }
   }
 
   public onValueChange(msg) {
-    this._store.dispatch(new fromAccount.UpdateAccount());
     const data = { id: msg.id, onClose: true };
-    this.api.onCloseNotify(data).subscribe(
-      () => {
-        this.user = this.user.filter(el => el.id !== msg.id);
-        this._store.dispatch(new fromAccount.UpdateAccountSuccess(this.user));
-      },
-      err => {
-        this._store.dispatch(new fromAccount.UpdateAccountFail(err));
-      }
-    );
+    this.api.onCloseNotify(data).subscribe(() => {
+      this.user = this.user.filter(el => el.id !== msg.id);
+      this.sessionService.changingUser.emit(this.user);
+    });
   }
 
   private _mapMessages(msg) {
