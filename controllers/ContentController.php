f987707366e773165832fa0e019b97cb387716cd
Louis Walter <louis.walter0725@gmail.com>
2020-03-26 19:53:44 +0500

    Сделан функционал, дял получение каталога материалов с фильтрами и пагинацией

--- controllers/ContentController.php
+++ controllers/ContentController.php
@@ -5,16 +5,60 @@ namespace app\controllers;
 
 use app\models\Category;
 use app\models\Content;
-use app\models\ContentCategory;
 use Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
 
 class ContentController extends ActiveController {
     public $modelClass = Content::class;
 
     public function actions() {
         $actions = parent::actions();
-        unset($actions['create']);
+        unset($actions['index'], $actions['create']);
         return $actions;
+    }
+
+    /**
+     * @return ActiveDataProvider
+     * @throws NotFoundHttpException
+     */
+    public function actionIndex() {
+        $filter = Yii::$app->request->queryParams;
+
+        $query = Content::find();
+
+        if (array_key_exists('title', $filter) && $filter['title'] != 'undefined' && $filter['title'] != '') {
+            $query->andWhere(['like', 'title', urldecode($filter['title'])]);
+        }
+
+        if (array_key_exists('type', $filter) && $filter['type'] != 'undefined' && $filter['type'] != '') {
+            $query->andWhere(['type_content' => (int)$filter['type']]);
+        }
+
+        if (array_key_exists('complication', $filter) && $filter['complication'] != 'undefined' &&
+            $filter['complication'] != '') {
+            $query->andWhere(['level_JLPT' => $filter['complication']]);
+        }
+
+        if (array_key_exists('volume', $filter) && $filter['volume'] != 'undefined' && $filter['volume'] != '') {
+            $arrV = explode(',', $filter['volume']);
+            if ($arrV[1] != 'unlimited') {
+                $query->andWhere(['between', 'count_symbol', (int)$arrV[0], (int)$arrV[1]]);
+            } else {
+                $query->andWhere(['>=', 'count_symbol', (int)$arrV[0]]);
+            }
+        }
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query
+        ]);
+
+
+        if ($dataProvider->getTotalCount() == 0) {
+            throw new NotFoundHttpException('Materials not found');
+        }
+
+        return $dataProvider;
     }
 
     public function actionCreate() {
@@ -23,7 +67,6 @@ class ContentController extends ActiveController {
         if ($content->validate()) {
             $content->save();
 
-            Yii::info($content);
             if (!empty($content->category)) {
                 $categories = Category::find()->where(['id' => $content->category])->all();
 
