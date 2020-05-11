f1034f0e54447cdc8c3b3f199228e1f3be6a8fee
Louis Walter <louis.walter0725@gmail.com>
2020-05-11 14:08:01 +0500

    Поправлена строка которая подаётся для разбивки и перевода

--- controllers/TranslateController.php
+++ controllers/TranslateController.php
@@ -24,18 +24,25 @@ class TranslateController extends ActiveController {
         $str_search = '';
         $filter = Yii::$app->getRequest()->getBodyParams();
         $meCab = new meCab();
-        $str_arTranslate = $meCab->analysis($filter['all_text']);
+        $str_arTranslate = $meCab->analysis(preg_replace('/\s+/', '', $filter['all_text']));
 
+        Yii::info('$str_arTranslate: ');
+        Yii::info($str_arTranslate);
         foreach ($str_arTranslate as $str) {
+            Yii::info('$str: ' . $str->getText());
             $str_offset = (int)iconv_strlen($str->getText());
+            Yii::info('$str_offset: ' . $str_offset);
             if ($offset + $str_offset <= $filter['offset']) {
                 $offset += (int)$str_offset;
+                Yii::info('$offset: ' . $offset);
             } else {
                 $str_search = $str->getText();
+                Yii::info('$str_search: ' . $str_search);
                 break;
             }
         }
 
+        Yii::info($str_search);
         if ($str_search != '') {
             $query = DictionaryWord::find()->where(['&^', 'query', $str_search])->asArray()->limit(100)->all();
             return [
