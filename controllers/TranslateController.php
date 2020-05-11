1d3df55709da1b56f37ed0486deef3c273652100
Louis Walter <louis.walter0725@gmail.com>
2020-05-11 14:11:48 +0500

    Убрано логирование в переводе

--- controllers/TranslateController.php
+++ controllers/TranslateController.php
@@ -26,23 +26,16 @@ class TranslateController extends ActiveController {
         $meCab = new meCab();
         $str_arTranslate = $meCab->analysis(preg_replace('/\s+/', '', $filter['all_text']));
 
-        Yii::info('$str_arTranslate: ');
-        Yii::info($str_arTranslate);
         foreach ($str_arTranslate as $str) {
-            Yii::info('$str: ' . $str->getText());
             $str_offset = (int)iconv_strlen($str->getText());
-            Yii::info('$str_offset: ' . $str_offset);
             if ($offset + $str_offset <= $filter['offset']) {
                 $offset += (int)$str_offset;
-                Yii::info('$offset: ' . $offset);
             } else {
                 $str_search = $str->getText();
-                Yii::info('$str_search: ' . $str_search);
                 break;
             }
         }
 
-        Yii::info($str_search);
         if ($str_search != '') {
             $query = DictionaryWord::find()->where(['&^', 'query', $str_search])->asArray()->limit(100)->all();
             return [
