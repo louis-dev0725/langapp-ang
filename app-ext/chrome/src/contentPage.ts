e80120b613a1837dc79f5627774cbb6e2651ebae
Louis Walter <louis.walter0725@gmail.com>
2020-05-27 14:11:20 +0500

    Доработки сделаны

--- app-ext/chrome/src/contentPage.ts
+++ app-ext/chrome/src/contentPage.ts
@@ -22,6 +22,9 @@ let dictionaryWord = {
     url: null
 };
 
+const modalShadowElement = document.createElement('div');
+const modalShadowRoot = modalShadowElement.attachShadow({mode: 'open'});
+
 const modal = document.createElement('div');
 const mHeader = document.createElement('div');
 const mBody = document.createElement('div');
@@ -93,45 +96,25 @@ function createButtonListener() {
   document.addEventListener('dblclick', (e) => {
     if (setting === 'extension.DoubleClick') {
       if ((e.metaKey === false || e.ctrlKey === false) && e.shiftKey === false && e.altKey === false) {
-        statusModal = document.getElementById('modalTranslate');
-        if (statusModal === null) {
-          createModal();
-        }
-
-        innerTranslateObject(document.caretRangeFromPoint(e.x, e.y), user, e.pageX, e.pageY);
+        createAndSendData(e);
       }
     }
 
     if (setting === 'extension.DoubleClickCtrl') {
       if ((e.metaKey === true || e.ctrlKey === true) && e.shiftKey === false && e.altKey === false) {
-        statusModal = document.getElementById('modalTranslate');
-        if (statusModal === null) {
-          createModal();
-        }
-
-        innerTranslateObject(document.caretRangeFromPoint(e.x, e.y), user, e.pageX, e.pageY);
+        createAndSendData(e);
       }
     }
 
     if (setting === 'extension.DoubleClickShift') {
       if ((e.metaKey === false || e.ctrlKey === false) && e.shiftKey === true && e.altKey === false) {
-        statusModal = document.getElementById('modalTranslate');
-        if (statusModal === null) {
-          createModal();
-        }
-
-        innerTranslateObject(document.caretRangeFromPoint(e.x, e.y), user, e.pageX, e.pageY);
+        createAndSendData(e);
       }
     }
 
     if (setting === 'extension.DoubleClickAlt') {
       if ((e.metaKey === false || e.ctrlKey === false) && e.shiftKey === false && e.altKey === true) {
-        statusModal = document.getElementById('modalTranslate');
-        if (statusModal === null) {
-          createModal();
-        }
-
-        innerTranslateObject(document.caretRangeFromPoint(e.x, e.y), user, e.pageX, e.pageY);
+        createAndSendData(e);
       }
     }
   });
@@ -140,18 +123,29 @@ function createButtonListener() {
     if ((e.metaKey === true || e.ctrlKey === true) && e.shiftKey === false && e.altKey === false) {
       let selectedText = window.getSelection().toString().replace("\n", ' ');
       if (selectedText.length > 0) {
-        statusModal = document.getElementById('modalTranslate');
+        statusModal = modalShadowRoot.getElementById('modalTranslate');
         if (statusModal === null) {
           createModal();
         }
 
-        innerSelectedTranslateObject(selectedText, window.location, user, e.pageX, e.pageY);
+        innerSelectedTranslateObject(selectedText, window.location, user, document.caretRangeFromPoint(e.x, e.y));
       }
     }
   });
 }
 
+function createAndSendData(e) {
+  statusModal = modalShadowRoot.getElementById('modalTranslate');
+  if (statusModal === null) {
+    createModal();
+  }
+
+  innerTranslateObject(document.caretRangeFromPoint(e.x, e.y), user, e.pageY);
+}
+
 function createModal() {
+  document.body.appendChild(modalShadowElement);
+
   modal.setAttribute('id', 'modalTranslate');
   modal.style.position = 'absolute';
   modal.style.display = 'flex';
@@ -163,8 +157,9 @@ function createModal() {
   modal.style.background = '#fff';
   modal.style.borderRadius = '10px';
   modal.style.padding = '10px';
+  modal.style.border = '1px solid #000';
 
-  document.body.appendChild(modal);
+  modalShadowRoot.appendChild(modal);
 
   modal.appendChild(mHeader);
   modal.appendChild(mBody);
@@ -174,14 +169,19 @@ function createModal() {
 
   mHeader.innerHTML = '<button type="button" class="close" id="closeModal"><span>&times;</span></button>';
 
-  document.getElementById('closeModal').style.border = 'none';
-  document.getElementById('closeModal').style.background = 'transparent';
-  document.getElementById('closeModal').style.padding = '0.5rem 1rem';
-  document.getElementById('closeModal').style.fontSize = '1.5rem';
-  document.getElementById('closeModal').style.fontWeight = '700';
-  document.getElementById('closeModal').style.marginLeft = 'auto';
+  modalShadowRoot.getElementById('closeModal').style.border = 'none';
+  modalShadowRoot.getElementById('closeModal').style.background = 'transparent';
+  modalShadowRoot.getElementById('closeModal').style.padding = '0.5rem 1rem';
+  modalShadowRoot.getElementById('closeModal').style.fontSize = '1.5rem';
+  modalShadowRoot.getElementById('closeModal').style.fontWeight = '700';
+  modalShadowRoot.getElementById('closeModal').style.marginLeft = 'auto';
 
-  document.getElementById('closeModal').onclick = (() => {
+  modalShadowRoot.getElementById('closeModal').onclick = (() => {
+    mBody.innerHTML = '<ul id="list-translate"></ul>';
+    modal.style.display = 'none';
+  });
+
+  document.body.onclick = (() => {
     mBody.innerHTML = '<ul id="list-translate"></ul>';
     modal.style.display = 'none';
   });
@@ -190,7 +190,7 @@ function createModal() {
   mHeader.style.flexFlow = 'row nowrap';
   mHeader.style.width = '100%';
   mHeader.style.justifyContent = 'space-between';
-  mHeader.style.height = '45px';
+  mHeader.style.height = '60px';
 
   mBody.style.boxSizing = 'border-box';
   mBody.style.display = 'flex';
@@ -203,78 +203,7 @@ function createModal() {
   mBody.innerHTML = '<ul id="list-translate"></ul>';
 }
 
-function innerSelectedTranslateObject (selectedText, urlPage, user, offsetX, offsetY) {
-  selectedObj = null;
-
-  if (extensionSetting) {
-    selectedObj = {
-      text: selectedText,
-      url: urlPage.href
-    };
-
-    chrome.runtime.sendMessage({ type: 'sendSelectedBackground', data: selectedObj }, (response) => {
-      if (response.data.success) {
-        wordT.setAttribute('class', 'word-translate');
-        mHeader.insertBefore(wordT, document.getElementById('closeModal'));
-        wordT.style.textAlign = 'center';
-
-        if (response.data.res.length > 0) {
-          mBody.innerHTML = '<ul id="list-translate"></ul>';
-          wordT.innerHTML = '<span>' + JSON.parse(response.data.res[0].sourceData).kana[0].text + '</span>'
-              + '<h1 style="font-size:2em;text-align:center;">' + response.data.word + '</h1>';
-
-          let listTranslate = document.getElementById('list-translate');
-
-          response.data.res.forEach((res) => {
-            const transObj = JSON.parse(res.sourceData);
-
-            transObj.sense.forEach((sen) => {
-              sen.gloss.forEach((gl) => {
-                listTranslate.innerHTML += '<li style="padding-left:10px;border-bottom:1px solid #000;">'
-                    + '<a class="textDictionary" data-word="' + response.data.word
-                    + '" data-id="' + res.id + '" data-translate="' + gl.text + '">' + gl.text + '</a></li>';
-              });
-            });
-          });
-
-          let listDictionary = document.getElementsByClassName('textDictionary');
-          for (let i = 0; i < listDictionary.length; i++) {
-            listDictionary[i].addEventListener('click', () => {
-              addToDictionary(user, translateObj.url, translateObj.all_text,
-                  listDictionary[i].getAttribute('data-translate'),
-                  listDictionary[i].getAttribute('data-word'),
-                  listDictionary[i].getAttribute('data-id')
-              );
-            });
-          }
-
-          document.getElementById('list-translate').style.border = '1px solid #000';
-          document.getElementById('list-translate').style.borderRadius = '5px';
-          document.getElementById('list-translate').style.listStyle = 'none';
-          document.getElementById('list-translate').style.padding = '0';
-          document.getElementById('list-translate').style.borderBottom = 'none';
-        } else {
-          wordT.innerHTML = '<span></span><h1 style="font-size:2em;text-align:center;">' + response.data.word + '</h1>';
-          mBody.innerHTML = '<h3>Перевод не найден... пока-что</h3>';
-        }
-
-        modal.style.top = parseInt(offsetY) + 15 + 'px';
-        modal.style.left = '0';
-        if (parseInt(offsetX) - 150 > 0) {
-          modal.style.left = parseInt(offsetX) - 150 + 'px';
-        }
-        modal.style.display = 'flex';
-      }
-    });
-  } else {
-    mBody.innerHTML = '<h3>Для использования расширения нужно авторизоваться в сервисе.</h3>';
-    modal.style.left = (window.innerWidth / 2 - 150) + 'px';
-    modal.style.top = '200px';
-    modal.style.display = 'flex';
-  }
-}
-
-function innerTranslateObject (range, user, offsetX, offsetY) {
+function innerTranslateObject (range, user, pageY) {
   translateObj = null;
 
   if (extensionSetting) {
@@ -286,8 +215,12 @@ function innerTranslateObject (range, user, offsetX, offsetY) {
 
     chrome.runtime.sendMessage({ type: 'sendBackground', data: translateObj }, (response) => {
       if (response.data.success) {
+        let new_range = new Range();
+        new_range.setStart(range.startContainer, response.data.offset);
+        let rect = new_range.getBoundingClientRect();
+
         wordT.setAttribute('class', 'word-translate');
-        mHeader.insertBefore(wordT, document.getElementById('closeModal'));
+        mHeader.insertBefore(wordT, modalShadowRoot.getElementById('closeModal'));
         wordT.style.textAlign = 'center';
 
         if (response.data.res.length > 0) {
@@ -295,7 +228,7 @@ function innerTranslateObject (range, user, offsetX, offsetY) {
           wordT.innerHTML = '<span>' + JSON.parse(response.data.res[0].sourceData).kana[0].text + '</span>'
               + '<h1 style="font-size:2em;text-align:center;">' + response.data.word + '</h1>';
 
-          let listTranslate = document.getElementById('list-translate');
+          let listTranslate = modalShadowRoot.getElementById('list-translate');
 
           response.data.res.forEach((res) => {
             const transObj = JSON.parse(res.sourceData);
@@ -320,22 +253,92 @@ function innerTranslateObject (range, user, offsetX, offsetY) {
             });
           }
 
-          document.getElementById('list-translate').style.border = '1px solid #000';
-          document.getElementById('list-translate').style.borderRadius = '5px';
-          document.getElementById('list-translate').style.listStyle = 'none';
-          document.getElementById('list-translate').style.padding = '0';
-          document.getElementById('list-translate').style.borderBottom = 'none';
+          modalShadowRoot.getElementById('list-translate').style.border = '1px solid #000';
+          modalShadowRoot.getElementById('list-translate').style.borderRadius = '5px';
+          modalShadowRoot.getElementById('list-translate').style.listStyle = 'none';
+          modalShadowRoot.getElementById('list-translate').style.padding = '0';
+          modalShadowRoot.getElementById('list-translate').style.borderBottom = 'none';
         } else {
           wordT.innerHTML = '<span></span><h1 style="font-size:2em;text-align:center;">' + response.data.word + '</h1>';
           mBody.innerHTML = '<h3>Перевод не найден... пока-что</h3>';
         }
 
-        modal.style.left = '0';
-        if (parseInt(offsetX) - 150 > 0) {
-          modal.style.left = parseInt(offsetX) - 150 + 'px';
+        console.log(rect);
+
+        modal.style.left = rect.x + 'px';
+        modal.style.top = pageY + 20 + 'px';
+        modal.style.display = 'flex';
+      }
+    });
+  } else {
+    mBody.innerHTML = '<h3>Для использования расширения нужно авторизоваться в сервисе.</h3>';
+    modal.style.left = (window.innerWidth / 2 - 150) + 'px';
+    modal.style.top = '200px';
+    modal.style.display = 'flex';
+  }
+}
+
+function innerSelectedTranslateObject (selectedText, urlPage, user, range) {
+  selectedObj = null;
+
+  if (extensionSetting) {
+    selectedObj = {
+      text: selectedText,
+      url: urlPage.href
+    };
+
+    chrome.runtime.sendMessage({ type: 'sendSelectedBackground', data: selectedObj }, (response) => {
+      if (response.data.success) {
+        let new_range = new Range();
+        new_range.setStart(range.startContainer, range.startOffset);
+        let rect = new_range.getBoundingClientRect();
+
+        wordT.setAttribute('class', 'word-translate');
+        mHeader.insertBefore(wordT, modalShadowRoot.getElementById('closeModal'));
+        wordT.style.textAlign = 'center';
+
+        if (response.data.res.length > 0) {
+          mBody.innerHTML = '<ul id="list-translate"></ul>';
+          wordT.innerHTML = '<span>' + JSON.parse(response.data.res[0].sourceData).kana[0].text + '</span>'
+              + '<h1 style="font-size:2em;text-align:center;">' + response.data.word + '</h1>';
+
+          let listTranslate = modalShadowRoot.getElementById('list-translate');
+
+          response.data.res.forEach((res) => {
+            const transObj = JSON.parse(res.sourceData);
+
+            transObj.sense.forEach((sen) => {
+              sen.gloss.forEach((gl) => {
+                listTranslate.innerHTML += '<li style="padding-left:10px;border-bottom:1px solid #000;">'
+                    + '<a class="textDictionary" data-word="' + response.data.word
+                    + '" data-id="' + res.id + '" data-translate="' + gl.text + '">' + gl.text + '</a></li>';
+              });
+            });
+          });
+
+          let listDictionary = document.getElementsByClassName('textDictionary');
+          for (let i = 0; i < listDictionary.length; i++) {
+            listDictionary[i].addEventListener('click', () => {
+              addToDictionary(user, translateObj.url, translateObj.all_text,
+                  listDictionary[i].getAttribute('data-translate'),
+                  listDictionary[i].getAttribute('data-word'),
+                  listDictionary[i].getAttribute('data-id')
+              );
+            });
+          }
+
+          modalShadowRoot.getElementById('list-translate').style.border = '1px solid #000';
+          modalShadowRoot.getElementById('list-translate').style.borderRadius = '5px';
+          modalShadowRoot.getElementById('list-translate').style.listStyle = 'none';
+          modalShadowRoot.getElementById('list-translate').style.padding = '0';
+          modalShadowRoot.getElementById('list-translate').style.borderBottom = 'none';
+        } else {
+          wordT.innerHTML = '<span></span><h1 style="font-size:2em;text-align:center;">' + response.data.word + '</h1>';
+          mBody.innerHTML = '<h3>Перевод не найден... пока-что</h3>';
         }
 
-        modal.style.top = parseInt(offsetY) + 15 + 'px';
+        modal.style.left = rect.x + 'px';
+        modal.style.top = rect.y + 20 + 'px';
         modal.style.display = 'flex';
       }
     });
