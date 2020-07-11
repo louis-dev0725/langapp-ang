1596af293f074410888be8e62b7ea10bd8cb29d0
Louis Walter <louis.walter0725@gmail.com>
2020-07-11 11:51:28 +0500

    Дал доступ неавторизированным пользователям, запись в лог

--- controllers/LogController.php
+++ controllers/LogController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 
 use app\models\SettingPlugin;
 use Yii;
+use yii\filters\AccessControl;
 
 class LogController extends ActiveController {
     public $modelClass = SettingPlugin::class;
@@ -16,6 +17,21 @@ class LogController extends ActiveController {
         $actions = parent::actions();
         unset($actions['create']);
         return $actions;
+    }
+
+	public function behaviors() {
+        $behaviors['access'] = [
+            'class' => AccessControl::class,
+            'except' => ['options'],
+            'rules' => [
+                [
+                    'allow' => true,
+                    'roles' => ['?', '@'],
+                ]
+            ],
+        ];
+
+        return $behaviors;
     }
 
     public function actionCreate() {
