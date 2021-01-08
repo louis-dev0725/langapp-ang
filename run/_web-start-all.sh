a2945dbc437b36a75eb7e98da08b5e82dfa7fffa
Louis Walter <louis.walter0725@gmail.com>
2021-01-08 16:17:21 +0900

    Use bash inside background processes for dev.
    Former-commit-id: 62b741883807f268e2519e1d96094d84f76d1f8d

--- run/_web-start-all.sh
+++ run/_web-start-all.sh
@@ -8,9 +8,9 @@ screen -wipe > /dev/null 2>&1
 # screen -c ../run/_screen.conf -dmSL "juman" -Logfile "juman.log" -t "juman" bash -c "trap 'echo gotsigint' INT; /app/run/_web-start-juman.sh; bash"
 
 #screen -c ../run/_screen.conf -dmSL "backend-ts" -Logfile "backend-ts.log" -t "backend-ts" bash -c "trap 'echo gotsigint' INT; /app/run/_web-start-backend-ts.sh; bash" >> screen_log.log 2>&1
-screen -c ../run/_screen.conf -dmSL "backend-ts" -Logfile "backend-ts.log" -t "backend-ts"
+screen -c ../run/_screen.conf -dmSL "backend-ts" -Logfile "backend-ts.log" -t "backend-ts" bash
 screen -S "backend-ts" -X stuff "/app/run/_web-start-backend-ts.sh\n"
 
 #screen -c ../run/_screen.conf -dmSL "frontend" -Logfile "frontend.log" -t "frontend" bash -c "trap 'echo gotsigint' INT; /app/run/_web-start-frontend.sh; bash" >> screen_log.log 2>&1
-screen -c ../run/_screen.conf -dmSL "frontend" -Logfile "frontend.log" -t "frontend"
+screen -c ../run/_screen.conf -dmSL "frontend" -Logfile "frontend.log" -t "frontend" bash
 screen -S "frontend" -X stuff "/app/run/_web-start-frontend.sh\n"
