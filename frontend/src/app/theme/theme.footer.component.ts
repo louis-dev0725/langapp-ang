f5ac84e1425694b91c32874e6e233e45e298197e
Louis Walter <louis.walter0725@gmail.com>
2021-05-18 21:05:01 +0900

    Update links in app footer.
    Former-commit-id: de1b5acb41aff44657d961b35442a13662b7c39a

--- frontend/src/app/theme/theme.footer.component.ts
+++ frontend/src/app/theme/theme.footer.component.ts
@@ -7,7 +7,7 @@ import { APP_NAME, APP_DATE_CREATED } from '@app/config/config';
     <div class="layout-footer">
       <span> {{ appName }} © {{ dateString }} </span>
       <span class="footer-text-right">
-        <a href="">{{ 'Public offer' | translate }}</a> / <a href="">{{ 'Privacy police' | translate }}</a>
+      <a href="/">{{ 'Home' | translate }}</a> / <a href="/docs/terms-of-use.pdf" target="_blank">{{ 'Terms of Use' | translate }}</a> / <a href="/docs/privacy-policy.pdf" target="_blank">{{ 'Privacy policy' | translate }}</a> / <a href="/docs/tokutei.pdf" target="_blank">特定商取引に関する法律に基づく表記</a>
       </span>
     </div>
   `
