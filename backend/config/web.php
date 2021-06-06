23e5e85137ef041686c61aadfa8dce6ef0e5d547
Louis Walter <louis.walter0725@gmail.com>
2021-06-06 12:10:21 +0000

    Add content reports.
    Former-commit-id: 53c51a092216f9046d55bc9e804366e7ad54dfea

--- backend/config/web.php
+++ backend/config/web.php
@@ -73,7 +73,7 @@ $config = [
                 [
                     'class' => 'yii\rest\UrlRule',
                     'controller' => ['user', 'transaction', 'category', 'content', 'language', 'translate',
-                        'plugin', 'dictionary', 'mnemonic', 'log'],
+                        'plugin', 'dictionary', 'mnemonic', 'log', 'content-report'],
                     'prefix' => 'api',
                     'patterns' => [
                         'PUT,PATCH {id}' => 'update',
---
+++ backend/migrations/m210603_153831_create_content_report_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%content_report}}`.
+ */
+class m210603_153831_create_content_report_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%content_report}}', [
+            'id' => $this->primaryKey(),
+            'contentId' => $this->integer()->notNull(),
+            'userId' => $this->integer()->notNull(),
+            'userText' => $this->text()->notNull()->defaultValue(''),
+            'moderatorId' => $this->integer()->notNull()->defaultValue(0),
+            'moderatorText' => $this->text()->notNull()->defaultValue(''),
+            'isProcessed' => $this->boolean()->notNull()->defaultValue(false),
+        ]);
+
+        $this->addForeignKey('fk-content_report-contentId-content-id', 'content_report', 'contentId', 'content', 'id',
+            'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk-content_report-userId-user-id', 'content_report', 'userId', 'users', 'id', 'CASCADE',
+            'CASCADE');
+        $this->addForeignKey('fk-content_report-moderatorId-user-id', 'content_report', 'moderatorId', 'users', 'id',
+            'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-content_report-moderatorId-user-id', 'content_report');
+        $this->dropForeignKey('fk-content_report-userId-user-id', 'content_report');
+        $this->dropForeignKey('fk-content_report-contentId-content-id', 'content_report');
+        $this->dropTable('{{%content_report}}');
+    }
+}
