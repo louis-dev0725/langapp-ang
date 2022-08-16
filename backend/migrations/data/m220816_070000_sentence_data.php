977b551263afc5bfbb3a5251de209706d67a09a5
Louis Walter <louis.walter0725@gmail.com>
2022-08-16 15:09:53 +0900

    Update dictionary data.
    Former-commit-id: 3b564ed1f59887af94ddb464b85f72027bb183f6

--- backend/migrations/data/m220816_070000_sentence_data.php
+++ backend/migrations/data/m220816_070000_sentence_data.php
@@ -1,6 +1,6 @@
 <?php
 
-class m220811_070000_sentence_data extends \app\components\DataMigration
+class m220816_070000_sentence_data extends \app\components\DataMigration
 {
     public function init()
     {
