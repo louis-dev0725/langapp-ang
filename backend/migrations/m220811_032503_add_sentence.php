61ad6b0132444f0cd435ffb5a5bb0d0fdfdf03cc
Louis Walter <louis.walter0725@gmail.com>
2022-08-16 14:43:25 +0900

    Update sentence table.
    Former-commit-id: 0db72d63ccf60a3eac7d5425395de529bb9353c7

--- backend/migrations/m220811_032503_add_sentence.php
+++ backend/migrations/m220811_032503_add_sentence.php
@@ -15,10 +15,14 @@ class m220811_032503_add_sentence extends Migration
         $this->execute('CREATE TABLE "public"."sentence" (
             id serial NOT NULL PRIMARY KEY,
             "hash" text NOT NULL DEFAULT \'\',
+            "wordId" int4 NOT NULL DEFAULT 0,
+            "source" text NOT NULL DEFAULT \'\',
+            "score" float4 NOT NULL DEFAULT 0.0,
             "text" text NOT NULL DEFAULT \'\',
             "translations" jsonb NOT NULL DEFAULT \'{}\',
             "needTranslation" boolean NOT NULL DEFAULT false
         )');
+        $this->execute('CREATE INDEX "sentence_wordId_idx" ON "public"."sentence" ("wordId")');
         // $this->execute('CREATE INDEX "sentence_text_idx" ON "public"."sentence" ("hash")');
     }
 
